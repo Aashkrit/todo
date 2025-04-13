3e50926633ad0c0c42e80d90044fad56c97b5b1b
Aashkrit <ashkrit00780@gmail.com>
2025-04-13 14:29:55 +0530

    this is checking the gitignore

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 // this is the starting of the todo web app
+const express = require("express")
+const app = express()
 
+
+
+app.listen(3000, (req,res)=>{
+    console.log("the app is running in the port 3000")
+})
